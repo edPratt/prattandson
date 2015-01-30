827f9f9b058d9b84eaac11dc1787a17309584d65
Lenny Pratt <Lenno598@gmail.com>
2015-01-29 21:04:34 -0500

    Added ajax to the contact form.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Set the footer height dynamically to the body height on resize
 var bumpIt = function() {  
       $('body').css('margin-bottom', $('footer#site-footer').height());
     },
@@ -14,3 +15,27 @@ setInterval(function() {
     bumpIt();
   }
 }, 250);
+
+
+// Contact form Ajax
+var $contact_form_container = $("#contact-form-container");
+var $contact_form = $("#contact-form");
+var $contact_submit = $("#contact-submit");
+$contact_form.submit(function(e) {
+    e.preventDefault();
+    console.log("Contact form submit clicked.");
+    $contact_submit.attr("disabled", true);
+    $contact_form_container.fadeOut(function() {
+    $.ajax({
+        url: "http://formspree.io/lenny@prattdev.net",
+        method: "POST",
+        data: $contact_form.serializeArray(),
+        dataType: "json"
+    }).done(function(result) {
+        // Display a thank you message
+        $contact_form_container.load("/snippets/contact_thank_you.html", function() {
+            $contact_form_container.fadeIn("slow");
+        });
+    });
+    });
+});
